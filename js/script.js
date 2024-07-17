d56fad0ba770a65a30b55da17b557ec7ef3c9df4
Max Unanian <Zeptorqyiad99@gmail.com>
2024-07-18 01:15:21 +0500

    Started section hero in project

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let burgerList = document.querySelector('.burger__list')
+let burgerIcon = document.querySelector('.menu__icon')
+
+burgerIcon.addEventListener('click', () => {
+   if (burgerIcon) {
+      burgerList.classList.toggle('active')
+      document.body.classList.toggle('_lock')
+      burgerIcon.classList.toggle('active')
+   }
+})
+
+const swiper = new Swiper('.swiper', {
+   loop: true,
+   autoplay: {
+      disableOnInteraction: false,
+      delay: 5000,
+   },
+
+   pagination: {
+      el: '.swiper-pagination',
+   },
+
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+
+   scrollbar: {
+      el: '.swiper-scrollbar',
+   },
+})
